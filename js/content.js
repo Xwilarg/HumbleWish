6b36d01c88ed8f8e2be1a7d3b046ce80ad1d6456
Xwilarg <xwilarg@protonmail.com>
2022-04-14 15:16:27 +0200

    Display badge

--- js/content.js
+++ js/content.js
@@ -6,6 +6,18 @@ class Membership {
     get regex() {
         return new RegExp("https://www.humblebundle.com/membership/home");
     }
+
+    displayBadge(data) {
+        data.mainNode.parentNode.parentNode.parentNode.innerHTML +=
+        `<div class="humblewish-wishlisted">Wishlisted by ${data.ids.length} friend${(data.ids.length > 1 ? "s" : "")}</div>`;
+    }
+}
+
+class GameData {
+    constructor(node) {
+        this.ids = [];
+        this.mainNode = node;
+    }
 }
 
 let searchs = [
@@ -13,6 +25,8 @@ let searchs = [
 ];
 
 let parser = null;
+
+// Are we on a valid page
 for (let elem of searchs) {
     if (elem.regex.test(window.location.href)) {
         parser = elem;
@@ -21,22 +35,34 @@ for (let elem of searchs) {
 }
 
 if (parser !== null) {
-    let availableGames = [];
+    let availableGames = {};
 
+    // Get all games present on the page
     Array.from(document.getElementsByClassName(parser.className)).forEach(e => {
-        availableGames.push(e.innerHTML.trim());
+        availableGames[e.innerHTML.trim()] = new GameData(e);
     });
 
     chrome.storage.sync.get(null, function(elems) {
+        // Check each wishlist from cache
         elems.ids.split(',').forEach(e => {
             const id = e.trim();
             const wishGames = elems["content-" + id].split(';');
-            availableGames.forEach(game => {
-                if (wishGames.includes(game.replaceAll(";", ""))) {
-                    console.debug(`[${id}] Found ${game} in wishlist`);
+            for (let key in availableGames) {
+                // Cache store all games separated by a semi colon so we don't forget to remove it while comparing
+                if (wishGames.includes(key.replaceAll(";", ""))) {
+                    console.debug(`[${id}] Found ${key} in wishlist`);
+                    availableGames[key].ids.push(id);
                 }
-            });
+            }
         });
+
+        // For each game that was in a wishlist, we display a badge
+        for (let key in availableGames) {
+            const data = availableGames[key];
+            if (data.ids.length > 0) {
+                parser.displayBadge(data);
+            }
+        }
     });
 } else {
     console.debug("Page not supported by HumbleWish");
